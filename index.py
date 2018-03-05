7d2e09ed20a85b11dc8e56381deadd750a5e56c1
jluo9612 <luojiabei@gmail.com>
2018-03-04 18:54:27 -0800

    This api sucks :(

--- index.py
+++ index.py
@@ -5,49 +5,57 @@ from os.path import basename
 
 client = Algorithmia.client(config.key)
 algo = client.algo("cv/FaceRecognition/0.2.0")
-
 files = client.dir("data://jl9612/CommunityFaceClassifiers")
 
+# list namespace and people in model
+nm = {
+	"action": "list_name_spaces",
+	"data_collection": "CommunityFaceClassifiers"
+}
 
-# input = {
-# 	"action": "remove_name_space",
-# 	"data_collection": "CommunityFaceClassifiers",
-#  	"name_space": "cast"
-# }
+print (algo.pipe(nm).result)
 
-# print (algo.pipe(input).result)
+ppl = {
+	"action": "list_people",
+	"data_collection": "CommunityFaceClassifiers",
+ 	"name_space": "cast"
+}
+
+print (algo.pipe(ppl).result)
 
 #step 1 add images with labels (names)
 
-train = {
-	"action": "add_images",
-	"data_collection": "CommunityFaceClassifiers",
-	"name_space": "cast",
-	"images": []
-}
+# train = {
+# 	"action": "add_images",
+# 	"data_collection": "CommunityFaceClassifiers",
+# 	"name_space": "cast",
+# 	"images": []
+# }
 
-for file in files.files():
-	if "test" not in file.path:
-		data = {}
-		data["url"] = "data://" + file.path
-		name = os.path.splitext(basename(file.path))[0]
-		data["person"] = ''.join([i for i in name if not i.isdigit()])  # extracts name from path
-		train["images"].append(data)
+# for file in files.files():
+# 	if "test" not in file.path:
+# 		data = {}
+# 		data["url"] = "data://" + file.path
+# 		name = os.path.splitext(basename(file.path))[0]
+# 		data["person"] = ''.join([i for i in name if not i.isdigit()])  # extracts name from path
+# 		train["images"].append(data)
 
-print (algo.pipe(train).result)
+# print (train["images"])
+# print (algo.pipe(train).result)
 
 #step 2 predict with unlabeled image visualized
 
 test = {
 	"name_space": "cast",
-	"data_collection": "CommunityFaceClassifiers",
 	"action": "predict",
-	"images": [
-		{
-			"url": "data://jl9612/CommunityFaceClassifiers/community_wall1.jpg",
-			"output": "data://jl9612/CommunityFaceClassifiers/temp/community_visualized.png"
-		}
-	]
+	"data_collection": "CommunityFaceClassifiers",
+	"images": []
 }
 
-print (algo.pipe(test).result)
+for file in files.files():
+	if "test" in file.path:
+		data = {}
+		data["url"] = "data://" + file.path
+		test["images"].append(data)
+
+print (algo.pipe(test).result) #kept getting array error
